282ad9add6acefe6727a6d4822d2d4ac27ba5ee6
AJnonimus <semaestre2003@gmail.com>
2025-05-21 21:30:27 +0200

    listaAsistentes

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -9,21 +9,24 @@ import WorkoutPage from "./Pages/WorkoutPage";
 import { WorkoutProvider } from "./context/WorkoutContext";
 import GroupClassesPage from "./Pages/GroupClassesPage"; // importa tu página
 import { GroupClassProvider } from "./Context/GroupClassesContext"; // importa tu contexto
+import AssistantListPage from "./Pages/AssistantListPage"; 
+
 
 function App() {
   return (
     <AuthProvider>
       <WorkoutProvider>
         <GroupClassProvider>
-          <Routes>
+          <Routes>  
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route element={<ProtectedRoute />}>
-              <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/profile" element={<ProfilePage />} />
             </Route>
             <Route path="/my-workouts-by-day/:date" element={<WorkoutPage />} />
             <Route path="/group-classes-by-day/:date" element={<GroupClassesPage />} />
+            <Route path="/group-class/:id/attendees" element={<AssistantListPage />} />
           </Routes>
         </GroupClassProvider>
       </WorkoutProvider>
