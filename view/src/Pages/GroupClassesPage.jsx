7f23adce8ca1939edea2615475be9c7f409ab619
I02eccad <i02eccad@uco.es>
2025-05-26 21:10:07 +0200

    pequeño ajuste del boton de añadir ejercicio

--- view/src/Pages/GroupClassesPage.jsx
+++ view/src/Pages/GroupClassesPage.jsx
@@ -276,12 +276,14 @@ const handleDeleteClass = async (classId) => {
               <p className="text-sm text-gray-500">No hay clases para este día.</p>
             )}
             </div>
+            {user?.role === "trainer" && (
             <button
               className="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded-full shadow-lg text-l"
               onClick={() => navigate("/create-group-class")}
               >
               + Añadir Clase
             </button>
+            )}
           </div>
           {/* Flecha derecha */}
           <div className="flex flex-col items-start justify-start">
